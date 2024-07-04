517b2b133113259514de2bb283a2e8b103ae7ad3
abdul moiz hussain <moizh750@gmail.com>
2024-07-04 21:01:40 +0530

    express setup

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+const app = express();
+dotenv.config();
+app.use(cors({
+    origin: "*",
+    credentials: true
+}));
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: "server connected"
+    });
+});
+app.listen(process.env.PORT, () => {
+    console.log("connected on port ", process.env.PORT);
+});
